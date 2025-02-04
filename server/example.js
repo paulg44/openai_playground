dbf699ba1187f29f011fde935358e99403225043
Paul Garton <paulgarton1984@hotmail.com>
2025-02-04 19:29:42 +0000

    analyse an image

--- server/example.js
+++ server/example.js
@@ -4,15 +4,27 @@ import OpenAI from "openai";
 dotenv.config();
 
 const apiKey = process.env.OPENAI_API_KEY;
+if (!apiKey) {
+  console.error("API key not set. Check your .env file.");
+  process.exit(1);
+}
+
 const openai = new OpenAI({ apiKey });
 
 const completion = await openai.chat.completions.create({
   model: "gpt-4o-mini",
   messages: [
-    { role: "system", content: "You are a helpful assistant." },
     {
       role: "user",
-      content: "Write a haiku about recursion in programming.",
+      content: [
+        { type: "text", text: "What's in this image?" },
+        {
+          type: "image_url",
+          image_url: {
+            url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+          },
+        },
+      ],
     },
   ],
   store: true,
